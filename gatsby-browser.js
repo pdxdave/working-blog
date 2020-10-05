49f8d2c81c08b1201452ffd494862973ef17c1d6
pdxdave <webdevdave@hotmail.com>
2020-10-04 18:48:29 -0700

    added global styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,13 +1,30 @@
 import React from 'react'
-import {ThemeProvider} from 'styled-components'
+import {createGlobalStyle, ThemeProvider} from 'styled-components'
 import Theme from './src/themes/theme'
+
+const GlobalStyles = createGlobalStyle `
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    body, html {
+        font-family: ${props => props.theme.fonts.main};
+        height: 100%;
+        background-color: ${props => props.theme.colors.light1}
+    }
+`
 
 export const wrapRootElement = ({element}) => (
     <ThemeProvider theme={Theme}>
+        <GlobalStyles />
         {element}
     </ThemeProvider>
 )
 
 /*
 styled-components has full theming support by exporting a <ThemeProvider> wrapper component. This component provides a theme to all React components underneath itself via the context API. 
+
+GlobalStyles passes these styles through the entire react app.
 */
