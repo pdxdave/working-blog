91808ea8334427c9fd06df673b0b870f08a08e75
pdxdave <webdevdave@hotmail.com>
2020-10-06 11:10:06 -0700

    404 page

---
+++ src/pages/404.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {graphql} from 'gatsby'
+import {Container, Content, FeatureImage} from '../components'
+import {H1} from "../elements"
+
+
+const notFound = ({data}) => {
+    const featureImage = data.imageSharp.fixed
+
+    return (
+        <Container>
+            <FeatureImage fixed={featureImage} />
+            <Content>
+                <H1 textAlign="center" margin="0 0 1rem 0">You've hit a...</H1>
+            </Content>
+        </Container>
+    )
+}
+
+export default notFound 
+
+export const notFoundQuery = graphql`
+    query NotFoundQuery {
+        imageSharp(fixed: {originalName: {eq: "deadend.jpg" }}) {
+            fixed (width: 1200, quality:100){
+                ...GatsbyImageSharpFixed
+            }
+        }
+    }
+`
